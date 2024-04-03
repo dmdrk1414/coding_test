774d9e5c3a5956bc71804030b192a5a5de836ce7
박승찬 <seungchan141414@gmail.com>
2024-04-04 02:27:35 +0900

    add 15649
    https://www.acmicpc.net/problem/15651

--- src/main/java/org/example/exhaustive_search/Back15649.java
+++ src/main/java/org/example/exhaustive_search/Back15649.java
@@ -24,10 +24,10 @@ public class Back15649 {
   }
 
   // Recurrence Function (재귀 함수)
-  // 만약 M 개를 전부 고름   => 조건에 맞는 탐색을 한 가지 성공한 것!
+  // 만약 M 개를 전부 고름   =>   조건에 맞는 탐색을 한 가지 성공한 것!
   // 아직 M 개를 고르지 않음 => k 번째부터 M번째 원소를 조건에 맞게 고르는 모든 방법을 시도한다.
   static void rec_func(int k) {
-    
+
     if (k == M + 1) { // 다 골랐다!
       // selected[1...M] 배열이 새롭게 탐색된 결과
       for (int i = 1; i <= M; i++) {
