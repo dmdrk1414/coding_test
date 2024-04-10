a0c6cbe65eeeeb9ab4ada16ca53fab4721aa5e7e
박승찬 <seungchan141414@gmail.com>
2024-04-10 21:53:38 +0900

    add

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,29 +1,78 @@
-import java.io.*;
-import java.util.Arrays;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
 
 public class Main {
 
-  public static void main(String[] args) throws IOException {
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    int n = Integer.parseInt(br.readLine());
-    int[][] dp = new int[n + 1][10];
-    int MOV = 10007;
+  static BufferedReader br;
+  static StringTokenizer st;
+  static int max_value = 100000 * 4;
+  static int[][] dir = {{-1, 0}, {0, -1}, {1, 0}, {0, 1}};
+  static ArrayList<Integer> ary;
+  static int answer = max_value;
+  static int[][][] dp;
+  static int size;
 
-    for (int i = 0; i <= 9; i++) {
-      dp[0][i] = 1;
+  public static void main(String[] args) throws IOException {
+    br = new BufferedReader(new InputStreamReader(System.in));
+    ary = new ArrayList<Integer>();
+    ary.add(-1);
+    input();
+    run();
+    System.out.print(answer);
+    
+  }
+
+  public static void input() throws IOException {
+    st = new StringTokenizer(br.readLine());
+    int op = 0;
+    while (st.hasMoreTokens()) {
+      op = Integer.parseInt(st.nextToken());
+      if (op == 0) {
+        break;
+      }
+      ary.add(op);
     }
-    for (int k = 1; k <= n; k++) { // 자릿수
-      for (int i = 0; i <= 9; i++) { // 큰곳의 숫자.
-        for (int j = i; j <= 9; j++) {
-          dp[k][i] += (dp[k - 1][j] % MOV);
+    size = ary.size();
+    dp = new int[5][5][size]; // dp 배열 : 왼쪽발, 오른쪽발, 입력받은 명령의 수
+    for (int i = 0; i < 5; i++) {
+      for (int j = 0; j < 5; j++) {
+        for (int k = 0; k < size; k++) {
+          dp[i][j][k] = max_value;
         }
       }
     }
-    
-    int result = 0;
-    for (int i = 0; i <= 9; i++) {
-      result += dp[n - 1][i];
+  }
+
+  public static void run() {
+    dp[0][0][0] = 0;
+    for (int idx = 1; idx < size; idx++) {
+      int op = ary.get(idx); // 옮길 발의 위치.
+      for (int i = 0; i < 5; i++) {
+        for (int j = 0; j < 5; j++) {
+          dp[i][op][idx] = Math.min(dp[i][op][idx], dp[i][j][idx - 1] + moveCost(j, op));
+          dp[op][j][idx] = Math.min(dp[op][j][idx], dp[i][j][idx - 1] + moveCost(i, op));
+        }
+      }
     }
-    System.out.println(dp[n][0] % 10007);
+    int op = ary.get(size - 1);
+    for (int i = 0; i < 5; i++) {
+      answer = Math.min(answer, dp[op][i][size - 1]);
+      answer = Math.min(answer, dp[i][op][size - 1]);
+    }
+  }
+
+  public static int moveCost(int pastOp, int nowOp) {
+    if (pastOp == 0) { // 0에서 뻗어나가면 비용은 2
+      return 2;
+    } else if (pastOp == nowOp) { //발판이 같다면 비용은 1
+      return 1;
+    } else if ((pastOp + nowOp) % 2 == 0) { // 반대방향은 비용이 4
+      return 4;
+    } else { // 인접방향은 비용이3
+      return 3;
+    }
   }
 }
