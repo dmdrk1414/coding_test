281aa982a3c22327f18b9187931fbf1a45d85741
dmdrk1414 <seungchan141414@gmail.com>
2024-04-20 18:07:56 +0900

    add

--- src/main/java/org/example/bfsdfs/back2667.java
+++ src/main/java/org/example/bfsdfs/back2667.java
@@ -1,8 +1,16 @@
-import java.io.*;
-import java.lang.reflect.Array;
-import java.util.*;
+package org.example.bfsdfs;
 
-public class Main {
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.StringTokenizer;
+
+public class back2667 {
 
   static FastReader scan = new FastReader();
   static StringBuilder sb = new StringBuilder();
