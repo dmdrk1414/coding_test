fb14960752813845b0b3c3820d0153982203531c
박승찬 <seungchan141414@gmail.com>
2024-04-22 14:30:53 +0900

    Back1806

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,182 +4,148 @@ import java.util.*;
 
 public class Main {
 
-  static FastReader scan = new FastReader();
-  static StringBuilder sb = new StringBuilder();
+  /*
+5 4
+.D.*
+....
+..X.
+S.*.
+....
 
+   */
+  static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+  static int[][] water_board;
+  static int[][] ani_board;
+  static boolean[][] visited;
+  static String[] graph;
   static int N, M;
-  static String[] a;
-  static int[][] dist_water, dist_hedgehog;
-  static boolean[][] visit;
-  static int[][] dir = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
-  static ArrayList<Integer> group;
+  static int[][] dir = new int[][]{{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
 
-  static void input() {
-    N = scan.nextInt();
-    M = scan.nextInt();
-    a = new String[N];
+  static void input() throws IOException {
+    String[] input_arr = br.readLine().split(" ");
+    N = Integer.parseInt(input_arr[0]);
+    M = Integer.parseInt(input_arr[1]);
+    water_board = new int[N][M];
+    ani_board = new int[N][M];
+    visited = new boolean[N][M];
+    graph = new String[N];
     for (int i = 0; i < N; i++) {
-      a[i] = scan.nextLine();
+      graph[i] = br.readLine().trim();
     }
-    visit = new boolean[N][M];
-    dist_water = new int[N][M];
-    dist_hedgehog = new int[N][M];
   }
 
-  // 모든 물들을 시작으로 동시에 BFS 시작!
-  static void bfs_water() {
-    Queue<Integer> Q = new LinkedList<>();
-    // 모든 물의 위치를 Q에 전부 넣어주기!
+  static void water_bfs() {
+    Queue<Integer> q = new LinkedList<>();
     for (int i = 0; i < N; i++) {
       for (int j = 0; j < M; j++) {
-        dist_water[i][j] = -1;
-        visit[i][j] = false;
-        if (a[i].charAt(j) == '*') {
-          Q.add(i);
-          Q.add(j);
-          dist_water[i][j] = 0;
-          visit[i][j] = true;
+        water_board[i][j] = -1;
+        visited[i][j] = false;
+        if (graph[i].charAt(j) == '*') {
+          q.add(i);
+          q.add(j);
+          water_board[i][j] = 0;
+          visited[i][j] = true;
         }
       }
     }
 
-    // BFS 과정 시작
-    while (!Q.isEmpty()) {
-      int x = Q.poll();
-      int y = Q.poll();
-      for (int k = 0; k < 4; k++) {
-        int nx = x + dir[k][0], ny = y + dir[k][1];
-        if (nx < 0 || ny < 0 || nx >= N || ny >= M) {
-          continue;  // 지도를 벗어나는 곳으로 가는가?
+    while (!q.isEmpty()) {
+      int x = q.poll();
+      int y = q.poll();
+
+      for (int i = 0; i < dir.length; i++) {
+        int nx = x + dir[i][0];
+        int ny = y + dir[i][1];
+
+        if (nx < 0 || nx >= N || ny < 0 || ny >= M) {
+          continue;
         }
-        if (a[nx].charAt(ny) != '.') {
-          continue;  // 갈 수 있는 칸인지 확인해야 한다.
+        if (graph[nx].charAt(ny) != '.') {
+          continue;
         }
-        if (visit[nx][ny]) {
-          continue;  // 이미 방문한 적이 있는 곳인가?
+        if (visited[nx][ny]) {
+          continue;
         }
-        Q.add(nx);
-        Q.add(ny);
-        visit[nx][ny] = true;
-        dist_water[nx][ny] = dist_water[x][y] + 1;
+        q.add(nx);
+        q.add(ny);
+        visited[nx][ny] = true;
+        water_board[nx][ny] = water_board[x][y] + 1;
       }
     }
   }
 
-  // 고슴도치를 시작으로 동시에 BFS 시작!
-  static void bfs_hedgehog() {
-    Queue<Integer> Q = new LinkedList<>();
-    // 고슴도치 위치를 Q에 넣어주기!
+  private static void ani_bfs() {
+    Queue<Integer> q = new LinkedList<>();
     for (int i = 0; i < N; i++) {
       for (int j = 0; j < M; j++) {
-        dist_hedgehog[i][j] = -1;
-        visit[i][j] = false;
-        if (a[i].charAt(j) == 'S') {
-          Q.add(i);
-          Q.add(j);
-          dist_hedgehog[i][j] = 0;
-          visit[i][j] = true;
+        ani_board[i][j] = -1;
+        visited[i][j] = false;
+        if (graph[i].charAt(j) == 'S') {
+          q.add(i);
+          q.add(j);
+          ani_board[i][j] = 0;
+          visited[i][j] = true;
         }
       }
     }
 
-    // BFS 과정 시작
-    while (!Q.isEmpty()) {
-      int x = Q.poll();
-      int y = Q.poll();
-      for (int k = 0; k < 4; k++) {
-        int nx = x + dir[k][0], ny = y + dir[k][1];
-        if (nx < 0 || ny < 0 || nx >= N || ny >= M) {
-          continue;  // 지도를 벗어나는 곳으로 가는가?
+    while (!q.isEmpty()) {
+      int x = q.poll();
+      int y = q.poll();
+
+      for (int i = 0; i < dir.length; i++) {
+        int nx = x + dir[i][0];
+        int ny = y + dir[i][1];
+
+        if (nx < 0 || nx >= N || ny < 0 || ny >= M) {
+          continue;
         }
-        if (a[nx].charAt(ny) != '.' && a[nx].charAt(ny) != 'D') {
-          continue;  // 갈 수 있는 칸인지 확인해야 한다.
+        if (graph[nx].charAt(ny) != '.' && graph[nx].charAt(ny) != 'D') {
+          continue;
         }
-        if (dist_water[nx][ny] != -1 && dist_water[nx][ny] <= dist_hedgehog[x][y] + 1) {
-          continue;  // 물에 잠기지는 않는가?
+        if (water_board[nx][ny] != -1 && ani_board[x][y] + 1 >= water_board[nx][ny]) {
+          continue;
         }
-        if (visit[nx][ny]) {
-          continue;  // 이미 방문한 적이 있는 곳인가?
+        if (visited[nx][ny]) {
+          continue;
         }
-        Q.add(nx);
-        Q.add(ny);
-        visit[nx][ny] = true;
-        dist_hedgehog[nx][ny] = dist_hedgehog[x][y] + 1;
+
+        q.add(nx);
+        q.add(ny);
+        visited[nx][ny] = true;
+        ani_board[nx][ny] = ani_board[x][y] + 1;
       }
     }
   }
 
   static void pro() {
-    // 각 칸마다 물에 닿는 시간 계산하기
-    bfs_water();
-
-    // 고슴도치가 물을 피해 탐색할 수 있는 공간 찾기
-    bfs_hedgehog();
-
-    // 탈출구 'D' 에 대한 결과를 통해 정답 출력하기
+    water_bfs();
+    ani_bfs();
     for (int i = 0; i < N; i++) {
       for (int j = 0; j < M; j++) {
-        if (a[i].charAt(j) == 'D') {
-          if (dist_hedgehog[i][j] == -1) {
+        if (graph[i].charAt(j) == 'D') {
+          if (ani_board[i][j] == -1) {
             System.out.println("KAKTUS");
           } else {
-            System.out.println(dist_hedgehog[i][j]);
+            System.out.println(ani_board[i][j]);
           }
         }
       }
     }
   }
 
-  public static void main(String[] args) {
-    input();
-    pro();
+
+  static void print(int[][] arr) {
+    for (int i = 0; i < N; i++) {
+      for (int j = 0; j < M; j++) {
+        System.out.print(arr[i][j] + " ");
+      }
+      System.out.println();
+    }
   }
 
-
-  static class FastReader {
-
-    BufferedReader br;
-    StringTokenizer st;
-
-    public FastReader() {
-      br = new BufferedReader(new InputStreamReader(System.in));
-    }
-
-    public FastReader(String s) throws FileNotFoundException {
-      br = new BufferedReader(new FileReader(new File(s)));
-    }
-
-    String next() {
-      while (st == null || !st.hasMoreElements()) {
-        try {
-          st = new StringTokenizer(br.readLine());
-        } catch (IOException e) {
-          e.printStackTrace();
-        }
-      }
-      return st.nextToken();
-    }
-
-    int nextInt() {
-      return Integer.parseInt(next());
-    }
-
-    long nextLong() {
-      return Long.parseLong(next());
-    }
-
-    double nextDouble() {
-      return Double.parseDouble(next());
-    }
-
-    String nextLine() {
-      String str = "";
-      try {
-        str = br.readLine();
-      } catch (IOException e) {
-        e.printStackTrace();
-      }
-      return str;
-    }
+  public static void main(String[] args) throws IOException {
+    input();
+    pro();
   }
 }
