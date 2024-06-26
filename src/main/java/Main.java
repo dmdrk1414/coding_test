fd4a6a4cf0f89f108a450a80f7c330fd1413696a
dmdrk1414 <seungchan141414@gmail.com>
2024-06-26 14:17:24 +0900

    add

---
+++ src/main/java/Main.java
@@ -0,0 +1,53 @@
+import java.io.*;
+import java.util.*;
+
+/**
+ 6
+ 10
+ 20
+ 15
+ 25
+ 10
+ 20
+
+ */
+
+public class Main {
+  static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+  static int N;
+  static int ONE = 0, TWO = 1;
+  static int[] arr;
+  static int[][] dp;
+
+  private static void input() throws IOException {
+    N = Integer.parseInt(br.readLine().trim());
+    arr = new int[N + 1];
+    dp = new int[N + 1][2];
+
+    for (int i = 1; i <= N; i++) {
+      arr[i] = Integer.parseInt(br.readLine().trim());
+    }
+    System.out.println("Arrays.toString(arr) = " + Arrays.toString(arr));
+  }
+
+  private static void pro() {
+    dp[1][ONE] = arr[1];
+    dp[1][TWO] = 0;
+
+    if (N >=2){
+      dp[2][ONE] = arr[1] + arr[2];
+      dp[2][TWO] = arr[2];
+    }
+
+    for (int i = 3; i <= N ; i++) {
+      dp[i][ONE] = dp[i - 1][TWO] + arr[i];
+      dp[i][TWO] = Math.max(dp[i - 2][ONE], dp[i - 2][TWO]) + arr[i];
+    }
+
+  }
+
+  public static void main(String[] args) throws IOException {
+    input();
+    pro();
+  }
+}
