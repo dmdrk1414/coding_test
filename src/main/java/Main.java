69788c58070f0b34c2afe4011d22252e5f5e0baa
dmdrk1414 <seungchan141414@gmail.com>
2024-08-16 21:45:09 +0900

    add

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,31 +1,149 @@
-class Parent {
-    int x, y;
+import java.io.*;
+import java.util.*;
 
-    Parent(int x, int y) {
-        this.x=x;
-        this.y=y;
+/**
+ 8
+ 5 40
+ 35 25
+ 10 20
+ 10 25
+ 30 50
+ 50 60
+ 30 25
+ 80 100
+ 30
+
+ 8
+ 5 40
+ 15 20
+ 10 20
+ 10 25
+ 30 50
+ 50 60
+ 30 25
+ 80 100
+ 30
+
+ 8
+ 5 10
+ 16 20
+ 15 21
+ 14 22
+ 13 33
+ 12 34
+ 11 45
+ 10 46
+ 30
+
+
+
+
+ 5
+ -5 -15
+ 30 40
+ -5 5
+ 50 40
+ 5 -5
+ 10
+
+ 10
+ -100000000 100000000
+ -100000000 100000000
+ -100000000 100000000
+ -100000000 100000000
+ -100000000 100000000
+ -100000000 100000000
+ -100000000 100000000
+ -100000000 100000000
+ -100000000 100000000
+ -100000000 100000000
+ 200000000
+
+ */
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static StringTokenizer st;
+    static int N, result, D;
+    static List<Info> list;
+    static PriorityQueue<Integer> pq;
+
+    private static void input() throws IOException {
+        st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        list = new ArrayList<>();
+        pq = new PriorityQueue<>();
+        result = 0;
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            list.add(new Info(Math.min(a, b), Math.max(a, b)));
+        }
+        st = new StringTokenizer(br.readLine());
+        D = Integer.parseInt(st.nextToken());
     }
 
-    int getT() {
-        return x*y;
-    }
-}
+    private static void pro() throws IOException {
+        Collections.sort(list);
 
-class Child extends Parent {
-    int x;
+        for (Info info : list)
+            System.out.println(info);
 
-    Child (int x) {
-        super(x+1, x);
-        this.x=x;
-    }
-    int getT(int n){
-        return super.getT()+n;
-    }
-}
+        for (int i = 0; i < N; i++) {
+            Info info = list.get(i);
+            if(info.r - info.l > D) continue;
+            pq.add(info.l);
 
-class Main {
-    public static void main(String[] args) {
-        Parent parent = new Child(3);
-        System.out.println(parent.getT());
+            while (!pq.isEmpty()) {
+                if(info.r - pq.peek() <= D) break;
+                pq.poll();
+            }
+
+            result = Math.max(result, pq.size());
+        }
+
+        bw.write(result + "");
+    }
+
+    public static void main(String[] args) throws IOException{
+        input();
+        pro();
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+
+    static class Info implements Comparable<Info> {
+        int l, r;
+
+        public Info(int l, int r) {
+            this.l = l;
+            this.r = r;
+        }
+
+        @Override
+        public int compareTo(Info o) {
+            if (this.r != o.r) {
+                return Integer.compare(this.r, o.r);
+            }
+
+            if (this.r == o.r) {
+                return Integer.compare(this.l, o.l);
+            }
+
+            return 0;
+        }
+
+        @Override
+        public String toString() {
+            return "{" +
+                    "l=" + l +
+                    ", r=" + r +
+                    '}';
+        }
     }
 }
