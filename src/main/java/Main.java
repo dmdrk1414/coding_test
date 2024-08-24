c977faab49ba5cf9b710e44cceab82b110ab13ff
dmdrk1414 <seungchan141414@gmail.com>
2024-08-24 16:40:21 +0900

    add

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,39 +1,40 @@
-import java.io.*;
-import java.util.*;
+import java.util.Scanner;
 
-/*
-7
-3
-7
-5
-2
-6
-1
-4
-*/
 public class Main {
-    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-    static StringTokenizer st;
-    static int result;
-    static StringBuilder sb;
 
-    private static void input() throws IOException {
-        result = 0;
-    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-    private static void pro() throws IOException {
-        totalProRec();
+        int N = sc.nextInt();  // 돌고래의 수
+        int K = sc.nextInt();  // 시간의 수
 
-    }
+        int[][] M = new int[N + 1][K + 1];
+        boolean[] camera = new boolean[N + 1];
+        boolean[] photographed = new boolean[N + 1];
 
-    public static void main(String[] args) throws IOException {
-        input();
-        pro();
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= K; j++) {
+                M[i][j] = sc.nextInt();  // 돌고래 i가 시간 j에 묘기
+            }
+        }
 
-        bw.write(result + "");
-        bw.flush();
-        bw.close();
-        br.close();
+        int cnt = 0;
+
+        for (int t = 1; t <= K; t++) {
+            for (int i = 1; i <= N; i++) {
+                if (M[i][t] == 1 && !photographed[i]) {  // 돌고래 i가 t시간 묘기를 && 촬영 XX
+                    if (!camera[i]) {  // 아직 카메라가 설치되지 않은 서식지라면
+                        camera[i] = true;  // 카메라 설치
+                        i--;
+                    } else {
+                        photographed[i] = true;  // 돌고래 촬영
+                        cnt++;
+                    }
+                }
+            }
+        }
+
+        System.out.println(cnt);
+        sc.close();
     }
 }
